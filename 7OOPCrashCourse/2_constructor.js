6a7c62bfd04be6cfc1e28b1cdb0289561578c94d
Aleksandr Maniunin <aamanunin@gmail.com>
2019-06-03 22:47:34 +0700

    add OOPcourse

---
+++ 7OOPCrashCourse/2_constructor.js
@@ -0,0 +1,14 @@
+// Constructor
+
+function Book(title, author, year) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.getSummary = function() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  }
+}
+
+const book1 = new Book('Book one', 'John Doe', '2013');
+const book2 = new Book('Book two', 'Jane doe', '2016');
+console.log(book1.getSummary());
